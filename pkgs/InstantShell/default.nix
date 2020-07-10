3a27fa987c9c45586cf41c1694f418a417372738
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2020-07-10 11:05:39 +0200

    cleaning muli source packages

--- pkgs/InstantShell/default.nix
+++ pkgs/InstantShell/default.nix
@@ -1,10 +1,9 @@
 { lib
 , stdenv
 , fetchFromGitHub
-, fetchurl
 , sqlite
 }:
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
 
   pname = "InstantShell";
   version = "unstable";
@@ -15,37 +14,29 @@ stdenv.mkDerivation rec {
       repo = "instantshell";
       rev = "2fcc83fea4c71b7537ca8b5ed3c0ea7147d79405";
       sha256 = "0iagkp0xpm0bbf3ajngwc8azhw7g97qkwg5q4wii6y7xibj958f0";
+      name = "instantshell";
     })
-    (fetchurl {
-      url = "https://github.com/ohmyzsh/ohmyzsh/archive/master.tar.gz";
-      sha256 = "17743dal38fvbja6r2xhvysc43h9s306iqcfcc23i601hzf2wa5i";
+    (fetchFromGitHub {
+      owner = "ohmyzsh";
+      repo = "ohmyzsh";
+      rev = "7deaff71a2be08145d83f0177edbf2dfb3e91262";
+      sha256 = "1xprm8wdyk1cb02mpl7dmnlxliww8v05nigb7qpgbxn42rww7891";
+      name = "ohmyzsh";
     })
   ];
 
-  sourceRoot = "source";
-
-  # unpackPhase = ''
-  #   runHook preUnpack
-  #   i=1
-  #   for _src in $srcs; do
-  #     echo $_src
-  #     # cp -r "$_src" source_$i
-  #     i=$((i+1))
-  #   done
-  #   runHook postUnpack
-  # '';
+  sourceRoot = ".";
 
   postPatch = ''
-    mv ../ohmyzsh-master ohmyzsh
-    ls -lh ohmyzsh/templates
-    cat zshrc >> ohmyzsh/templates/zshrc.zsh-template
-    rm zshrc
+    ls -lh
+    cat instantshell/zshrc >> ohmyzsh/templates/zshrc.zsh-template
+    rm instantshell/zshrc
   '';
 
   installPhase = ''
-    install -Dm 644 instantos.plugin.zsh $out/share/instantshell/custom/plugins/instantos/instantos.plugin.zsh
-    install -Dm 644 instantos.zsh-theme $out/share/instantshell/custom/themes/instantos.zsh-theme
-    install -Dm 555 install.sh $out/bin/instantshell
+    install -Dm 644 instantshell/instantos.plugin.zsh $out/share/instantshell/custom/plugins/instantos/instantos.plugin.zsh
+    install -Dm 644 instantshell/instantos.zsh-theme $out/share/instantshell/custom/themes/instantos.zsh-theme
+    install -Dm 555 instantshell/install.sh $out/bin/instantshell
     cp -r ohmyzsh/* $out/share/instantshell
   '';
 
