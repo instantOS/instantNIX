22d9ee43e62c5b8758cd627ef254ccce9df4870f
ppenguin <hieronymusv@gmail.com>
2021-03-30 14:45:19 +0200

    Update default.nix and default.nix: replace instantdata with fork and modify appmenu to allow for user themes

--- pkgs/instantData/default.nix
+++ pkgs/instantData/default.nix
@@ -21,10 +21,12 @@ stdenv.mkDerivation {
   version = "unstable";
 
   src = fetchFromGitHub {
-    owner = "SCOTT-HAMILTON";
+    # owner = "SCOTT-HAMILTON";
+    owner = "ppenguin";
     repo = "instantData";
-    rev = "ade1148cd26c272cb569f9a4c10337f5a4e91f29";
-    sha256 = "1207ihwxss7v3wz4xcvg21p10a8yz1r9phzb4n43138jpaynziqx";
+    # rev = "ade1148cd26c272cb569f9a4c10337f5a4e91f29";
+    rev = "e707dbdd7c99e948a9ab7e1c5b82496168f3d642";
+    sha256 = "0amlr04fk7r3azbq01lflf9izlzaaijdm19lcndi9bmfp8yci7xr";
     name = "instantOS_instantData";
   };
 
@@ -67,7 +69,7 @@ stdenv.mkDerivation {
   meta = with lib; {
     description = "instantOS Data";
     license = licenses.mit;
-    homepage = "https://github.com/instantOS/IinstantData";
+    homepage = "https://github.com/SCOTT-HAMILTON/InstantData";
     maintainers = [ "Scott Hamilton <sgn.hamilton+nixpkgs@protonmail.com>" ];
     platforms = platforms.linux;
   };
